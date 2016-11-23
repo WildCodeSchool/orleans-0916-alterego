e08a5289c69b5845ec8bec3cbf56f369a2bb13c1
QuentinChapelle <chapellequentin@live.fr>
2016-11-23 16:01:03 +0100

    création bundles. avec les bundles

---
+++ AlterEgo/src/CoachBundle/Controller/DefaultController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace CoachBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     */
+    public function indexAction()
+    {
+        return $this->render('CoachBundle:Default:index.html.twig');
+    }
+}

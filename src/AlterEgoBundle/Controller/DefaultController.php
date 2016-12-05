ec7f54908fa10e41c3c1d164209918a0d0ef3de5
Guillaume4545 <guillaumecuriel@gmail.com>
2016-12-05 14:47:15 +0100

    ajout de choses

--- src/AlterEgoBundle/Controller/DefaultController.php
+++ src/AlterEgoBundle/Controller/DefaultController.php
@@ -22,4 +22,12 @@ class DefaultController extends Controller
     {
         return $this->render('AlterEgoBundle:Default:coach.html.twig');
     }
+
+    /**
+     * @Route("/rh")
+     */
+    public function rhAction()
+    {
+        return $this->render('AlterEgoBundle:Default:rh.html.twig');
+    }
 }

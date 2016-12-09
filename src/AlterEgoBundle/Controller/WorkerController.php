382ff4f6f711965bdc68d8c81fd9489338aed458
gthielon <gthielon@gmail.com>
2016-12-09 11:27:20 +0100

    Ajout des pages worker

--- src/AlterEgoBundle/Controller/WorkerController.php
+++ src/AlterEgoBundle/Controller/WorkerController.php
@@ -15,4 +15,20 @@ class WorkerController extends Controller
         return $this->render('AlterEgoBundle:Worker:worker.html.twig');
     }
 
+    /**
+     * @Route("/worker/badges")
+     */
+    public function badgesAction()
+    {
+        return $this->render('AlterEgoBundle:Worker:badges.html.twig');
+    }
+
+    /**
+     * @Route("/worker/friends")
+     */
+    public function friendsAction()
+    {
+        return $this->render('AlterEgoBundle:Worker:friends.html.twig');
+    }
+
 }

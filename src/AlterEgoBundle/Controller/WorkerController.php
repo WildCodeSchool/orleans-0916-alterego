1e52552da0066aa415ec9d375baa55a09896a7b5
QuentinChapelle <chapellequentin@live.fr>
2017-01-06 11:12:57 +0100

    import page settings reste custom

--- src/AlterEgoBundle/Controller/WorkerController.php
+++ src/AlterEgoBundle/Controller/WorkerController.php
@@ -4,6 +4,10 @@ namespace AlterEgoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use FOS\RestBundle\Controller\Annotations\Route;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Model\UserInterface;
 
 /**
  * @Route("/worker", name="worker")
@@ -39,7 +43,25 @@ class WorkerController extends Controller
      */
     public function settingsAction()
     {
-        return $this->render('AlterEgoBundle:Worker:settings.html.twig');
+        $user = $this->container->get('security.context')->getToken()->getUser();
+        if (!is_object($user) || !$user instanceof UserInterface) {
+            throw new AccessDeniedException('This user does not have access to this section.');
+        }
+
+        $form = $this->container->get('fos_user.profile.form');
+        $formHandler = $this->container->get('fos_user.profile.form.handler');
+
+        $process = $formHandler->process($user);
+        if ($process) {
+            $this->setFlash('fos_user_success', 'profile.flash.updated');
+
+            return new RedirectResponse($this->getRedirectionUrl($user));
+        }
+
+        return $this->container->get('templating')->renderResponse(
+            'AlterEgoBundle:Worker:settings.html.'.$this->container->getParameter('fos_user.template.engine'),
+            array('form' => $form->createView())
+        );
     }
 
     /**
---
+++ web/assets/js/menu.js
@@ -0,0 +1,9 @@
+//# sourceMappingURL=menu.js.js.map
+
+    $('#check_nav').click(function(){
+        if( $('#check_nav').is(':checked') ){
+            $(".content").css({"transform" : "translateX(0)","transition-duration" : "0.6s","transition-delay" : "0s"})
+        } else {
+            $(".content").css({"transform" : "translateX(6vw)","transition-duration" : "0.6s","transition-delay" : "0.6s"})
+
+        }});

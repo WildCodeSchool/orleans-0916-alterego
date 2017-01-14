7c9067807ad320451928dfbe9f1de3a8fd41f9b0
QuentinChapelle <chapellequentin@live.fr>
2017-01-14 09:40:56 +0100

    modif controleur coach

--- src/AlterEgoBundle/Controller/CoachController.php
+++ src/AlterEgoBundle/Controller/CoachController.php
@@ -4,32 +4,113 @@ namespace AlterEgoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\DependencyInjection\ContainerAware;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Model\UserInterface;
+use AlterEgoBundle\Entity\Creneau;
+use AlterEgoBundle\Entity\Activite;
+use AlterEgoBundle\entity\Reservation;
+use Symfony\Component\HttpFoundation\Request;
+use AlterEgoBundle\Calendar\CalendarEvent;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\Validator\Constraints\DateTime;
+use AlterEgoBundle\Form\CheckType;
 
+/**
+ * @Route("/coach", name="coach")
+ */
 class CoachController extends Controller
 {
     /**
-     * @Route("/coach")
+     * @Route("/")
      */
-    public function coachAction()
+    public function coachAction(Request $request)
     {
-        return $this->render('AlterEgoBundle:Coach:coach.html.twig');
+        $user = $this->getUser();
+
+        $form = $this->createForm('AlterEgoBundle\Form\CheckType');
+        $form->handleRequest($request);
+
+        $em = $this->getDoctrine()->getManager();
+        $seances = $em->getRepository('AlterEgoBundle:Activite')->findByUser($user);
+
+        if ($seances) {
+            $date = new \DateTime();
+            foreach ($seances as $seance) {
+                if (!isset($nextSeance)) {
+                    $nextSeance = $seance;
+                }
+
+                if ($nextSeance->getActivite()->getDateheure() > $seance->getActivite()->getDateheure() && ($seance->getActivite()->getDateheure() >= $date)) {
+                    $nextSeance = $seance;
+                }
+            }
+
+            if ($form->isSubmitted() && $form->isValid()) {
+                $em = $this->getDoctrine()->getManager();
+                $em->persist($nextSeance);
+                $em->flush($seances);
+            }
+
+            return $this->render('AlterEgoBundle:Coach:coach.html.twig', array(
+                'seance' => $nextSeance,
+                'form' => $form->createView(),
+            ));
+
+        }
+
+        else {
+            return $this->render('AlterEgoBundle:Coach:coach.html.twig', array(
+                'seance' => $seances,));
+        }
+
+
+//        if($seances){
+//            $date = new \DateTime();
+//            foreach($seances as $seance) {
+//                if (!isset($nextSeance)) {
+//                    $nextSeance = $seance;
+//                }
+//                if ($nextSeance->getCreneau()->getDateheure() > $seance->getCreneau()->getDateheure() && ($seance->getCreneau()->getDateheure() >= $date)) {
+//                    $nextSeance = $seance;
+//                }
+//            }
+//
+//            if ($form->isSubmitted() && $form->isValid()) {
+//                $em = $this->getDoctrine()->getManager();
+//                $em->persist($nextSeance);
+//                $em->flush($nextSeance);
+//
+//            }
+//
+//            return $this->render('AlterEgoBundle:Coach:coach.html.twig', array(
+//                'seance' => $nextSeance,
+//                'form' => $form->createView(),
+//            ));
+//        } else {
+//
+//            return $this->render('AlterEgoBundle:Coach:coach.html.twig', array(
+//                'seance' => $seances,));
+//
+//        }
     }
-    
+
     /**
-     * @Route("/coach/activite")
+     * @Route("/activite")
      */
     public function coachActiviteAction()
-                    {
+    {
         return $this->render('AlterEgoBundle:Coach:coach_activite.html.twig');
     }
 
     /**
-     * @Route("/coach/planning")
+     * @Route("/planning")
      */
     public function coachPlanningAction()
     {
         $em = $this->getDoctrine()->getManager();
-        $activites =$em->getRepository('AlterEgoBundle:Activite')->findByUser($this->getUser());
+        $activites = $em->getRepository('AlterEgoBundle:Activite')->findByUser($this->getUser());
 
 
         return $this->render('AlterEgoBundle:Coach:planning.html.twig', array(

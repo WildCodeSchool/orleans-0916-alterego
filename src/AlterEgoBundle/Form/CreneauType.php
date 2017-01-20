7ea1cdc77fedf38af717adeb4406a52b05685bab
louri45 <l.richard45@gmail.com>
2017-01-20 14:09:43 +0100

    Style modif

--- src/AlterEgoBundle/Form/CreneauType.php
+++ src/AlterEgoBundle/Form/CreneauType.php
@@ -7,6 +7,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -17,14 +19,25 @@ class CreneauType extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder->add('lieu')
+        $builder->add('lieu', TextType::class ,array(
+                    'label_attr' => ['class' => 'label_creneau'],
+                    'label' => 'Lieu de la séance'
+                ))
                 ->add('dateheure', DateTimeType::class, array(
                     'widget' => 'single_text',
                     'html5' => false,
                     'attr' => ['class' => 'js-datepicker'],
+                    'label_attr' => ['class' => 'label_creneau'],
+                    'label' => 'Date et Heure de la séance',
                 ))
-                ->add('duree')
-                ->add('capacite');
+                ->add('duree', IntegerType::class, array(
+                    'label_attr' => ['class' => 'label_creneau'],
+                    'label' => 'Durée de la séance (min.)'
+                ))
+                ->add('capacite', IntegerType::class, array(
+                    'label_attr' => ['class' => 'label_creneau'],
+                    'label' => 'Capacité de la séance'
+                ));
     }
     
     /**

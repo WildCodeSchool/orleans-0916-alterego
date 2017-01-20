0d9a95ed96dbfaab0aa14cc045824bb89abd0529
louri45 <l.richard45@gmail.com>
2017-01-20 11:59:49 +0100

    DateTimePicker File

--- src/AlterEgoBundle/Controller/CreneauController.php
+++ src/AlterEgoBundle/Controller/CreneauController.php
@@ -60,7 +60,7 @@ class CreneauController extends Controller
             $creneau->setActivite($activite);
             $em->flush($creneau);
 
-            return $this->redirectToRoute('creneau_show', array('id' => $creneau->getId()));
+            return $this->redirectToRoute('creneau_edit', array('id' => $creneau->getId()));
         }
 
         return $this->render('creneau/new.html.twig', array(
@@ -75,10 +75,9 @@ class CreneauController extends Controller
      * @Route("/{id}", name="creneau_show")
      * @Method("GET")
      */
-    public function showAction(Creneau $creneau)
+    public function showAction(Request $request, Creneau $creneau)
     {
         $deleteForm = $this->createDeleteForm($creneau);
-
 
         return $this->render('creneau/show.html.twig', array(
             'creneau' => $creneau,
@@ -97,6 +96,10 @@ class CreneauController extends Controller
         $deleteForm = $this->createDeleteForm($creneau);
         $editForm = $this->createForm('AlterEgoBundle\Form\CreneauType', $creneau);
         $editForm->handleRequest($request);
+
+        $request->getSession()
+            ->getFlashBag()
+            ->add('success', 'Votre créneau a bien été créé !');
 
         if ($editForm->isSubmitted() && $editForm->isValid()) {
             $this->getDoctrine()->getManager()->flush();
